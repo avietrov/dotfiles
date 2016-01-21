9aadf32235a7c3d63ec8817316caf047865bb2aa
Anton Vietrov <avietrov@yahoo.de>
2016-01-21 17:22:46 +0100

    Remove broken aliases

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -10,15 +10,10 @@ alias o="open"
 alias vf='vifm .'
 alias speedtest='speedtest-cli'
 alias rm='trash'
-alias tv='terminal_velocity'
 
 # Recursively delete `.DS_Store` files
 alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
 
 # Reload the shell (i.e. invoke as a login shell)
 alias reload="exec $SHELL -l"
-
-# mvn
-alias mvni="mvn -Pscala-zinc install -DskipTests"
-alias mvnci="mvn -Pscala-zinc clean install -DskipTests"
 
