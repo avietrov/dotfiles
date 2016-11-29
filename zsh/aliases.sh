1295240ec2ce29c828e3e585d790ae0c16c2f14b
Anton Vietrov <avietrov@yahoo.de>
2016-11-30 00:50:08 +0100

    Alias vim to nvim

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -10,6 +10,7 @@ alias o="open"
 alias vf='vifm .'
 alias speedtest='speedtest-cli'
 alias rm='trash'
+alias vim='nvim'
 
 # Recursively delete `.DS_Store` files
 alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
