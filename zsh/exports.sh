5d86fbf3dcc317a4d7ef5493beb686cb76d58b01
Anton Vietrov <anton.vietrov@zalando.de>
2018-02-11 12:54:59 +0100

    Prioritize brew packages over system packages.

--- zsh/exports.sh
+++ zsh/exports.sh
@@ -12,6 +12,7 @@ export LESS_TERMCAP_md="${yellow}";
 export GREP_OPTIONS='--color=auto';
 
 export PATH="$PATH:/usr/local/sbin";
+export PATH="/usr/local/bin:$PATH";
 
 BASE16_SHELL=$HOME/.config/base16-shell/
 [ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
