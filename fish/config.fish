287f4a4c8592630a11505eeda28a9185db1543e2
Anton Vietrov <avietrov@users.noreply.github.com>
2022-02-12 16:44:21 +0100

    Migrate to fish.

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# PATH
+fish_add_path /usr/local/sbin
+
+# ALIASES
+alias reload='source ~/.config/fish/config.fish'
+alias rm='trash'
+alias vim='nvim'
+alias e='exa -l -h --git --no-permissions --no-user --icons --group-directories-first'
---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,5 @@
+function fish_prompt
+    echo (prompt_pwd)
+    set_color green
+    echo '>' (set_color normal)
+end
