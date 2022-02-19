2b4af1f080925ce27f59660abd755c486fe6a3d5
Anton Vietrov <avietrov@users.noreply.github.com>
2022-02-19 16:23:12 +0100

    Setup git aliases.

--- fish/config.fish
+++ fish/config.fish
@@ -12,3 +12,15 @@ alias vim='nvim'
 alias e='exa -l -h --git --no-permissions --no-user --icons --group-directories-first'
 
 source "$HOME/.config/custom.fish"
+
+# GIT aliases
+alias gs='git status -sb'
+alias gp='git push'
+alias gap='git add -p'
+alias gco='git commit -m'
+alias gpp='git push -u origin HEAD'
+alias gdi='git diff'
+alias glo='git log'
+alias gll='git log --oneline'
+alias gls='git log --stat'
+alias git_clean="git branch --merged master | grep -v master | xargs -n 1 git branch -d"
