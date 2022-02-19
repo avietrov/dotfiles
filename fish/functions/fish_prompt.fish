c8f62cf0969e7af0f8dadcc691fbf0b7967b0c32
Anton Vietrov <avietrov@users.noreply.github.com>
2022-02-19 16:23:12 +0100

    add location and git status to git prompt.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,5 +1,9 @@
 function fish_prompt
-    echo (prompt_pwd)
+    set_color yellow
+    echo -n (prompt_pwd)
+    set_color red
+    set -g __fish_git_prompt_show_informative_status true
+    echo (fish_git_prompt)
     set_color green
     echo '>' (set_color normal)
 end
