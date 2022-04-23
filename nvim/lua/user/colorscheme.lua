32d25e8c64f537f0285181ac8b7c7cf2c331e994
Anton Vietrov <avietrov@users.noreply.github.com>
2022-04-23 20:50:37 +0200

    Add colorscheme.

---
+++ nvim/lua/user/colorscheme.lua
@@ -0,0 +1,7 @@
+local colorscheme = "base16-monokai"
+
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+vim.cmd("colorscheme " .. colorscheme)
+vim.g.base16colorspace=256
+-- let g:lightline = { 'colorscheme': 'base16', }
--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -1,19 +1,9 @@
 vim.opt.compatible = false
-
--- LOOK AND FEEL
-vim.opt.termguicolors = true
-vim.opt.background = "dark"
--- let base16colorspace=256
--- let g:lightline = { 'colorscheme': 'base16', }
--- colorscheme base16-monokai
-
 vim.opt.number = true       -- Enable line numbers
 vim.opt.cursorline  = true  -- Highlight current line
 vim.opt.gdefault = true     -- Add the g flag to search/replace by default
 vim.opt.showmode  = false  -- Replaced by lightline
 vim.opt.backspace = {"indent","eol","start"} -- Allow backspace in insert mode
-
--- BEHAVIOR
 vim.opt.tabstop=2			-- Make tabs as wide as two spaces
 vim.opt.shiftwidth=2
 vim.opt.hlsearch=true 			-- Highlight searches
@@ -27,5 +17,4 @@ vim.opt.expandtab=true
 vim.opt.backup=false      -- no backups, vcs ftw
 vim.opt.wb=false
 vim.opt.swapfile= false
-
 vim.opt.relativenumber=true
