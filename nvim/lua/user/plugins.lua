2f6f79853fa76c355fb97ed9d6a50d2cebf9bd81
Anton Vietrov <avietrov@users.noreply.github.com>
2022-04-24 09:26:20 +0200

    Setup vim cmp.

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -36,6 +36,17 @@ return require('packer').startup(function()
   use 'scrooloose/nerdcommenter'
   use 'dag/vim-fish'
 
+  -- cmp plugins
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
   -- Git
   use 'airblade/vim-gitgutter'
   use 'Xuyuanp/nerdtree-git-plugin'
