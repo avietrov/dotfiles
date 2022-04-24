98b8b4131f3373afca8451b965318c065ecdc7bf
Anton Vietrov <avietrov@users.noreply.github.com>
2022-04-24 11:51:16 +0200

    Update nvim-tree icons.

--- nvim/lua/user/nvim-tree.lua
+++ nvim/lua/user/nvim-tree.lua
@@ -1,1 +1,29 @@
-require 'nvim-tree'.setup {}
+vim.g.nvim_tree_icons = {
+  default = "",
+  symlink = "",
+  git = {
+    unstaged = "",
+    staged = "S",
+    unmerged = "",
+    renamed = "➜",
+    deleted = "",
+    untracked = "U",
+    ignored = "◌",
+  },
+}
+
+require("nvim-tree").setup {
+  diagnostics = {
+    enable = true,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    },
+  },
+  trash = {
+    cmd = "trash",
+    require_confirm = true,
+  },
+}
