87d8d2b0a7eb9d0ef7b30968232b85580e692dd7
Anton Vietrov <avietrov@users.noreply.github.com>
2022-06-03 14:31:56 +0200

    update nvim tree config.

--- nvim/lua/user/nvim-tree.lua
+++ nvim/lua/user/nvim-tree.lua
@@ -1,18 +1,21 @@
-vim.g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-    unstaged = "",
-    staged = "S",
-    unmerged = "",
-    renamed = "➜",
-    deleted = "",
-    untracked = "U",
-    ignored = "◌",
-  },
-}
-
 require("nvim-tree").setup({
+  renderer = {
+    icons = {
+      glyphs = {
+        default = "",
+        symlink = "",
+        git = {
+          unstaged = "",
+          staged = "S",
+          unmerged = "",
+          renamed = "➜",
+          deleted = "",
+          untracked = "U",
+          ignored = "◌",
+        },
+      },
+    },
+  },
   diagnostics = {
     enable = true,
     icons = {
