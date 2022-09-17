1e9a6a21bb99730c16c92bf5407452f0e8f590d5
Anton Vietrov <avietrov@users.noreply.github.com>
2022-09-17 13:48:47 +0200

    Fix ingored fish prompt.

---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,11 @@
+function fish_prompt 
+  set_color $fish_color_cwd
+  echo -n (prompt_pwd)
+  set_color brblack
+  echo -n (fish_git_prompt)
+  echo
+  set_color blue
+  echo -n '❯'
+  set_color yellow
+  echo -n '❯ '
+end
