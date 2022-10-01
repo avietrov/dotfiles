4460a659ac513b8ba61fe748d54581f6d99ce4fb
Anton Vietrov <avietrov@users.noreply.github.com>
2022-10-01 15:18:07 +0200

    bufferline above buffer, skipping file tree.

--- nvim/lua/user/barbar.lua
+++ nvim/lua/user/barbar.lua
@@ -5,3 +5,22 @@ require('bufferline').setup {
   closable = false,
   clickable = true,
 }
+
+-- ensures tabs begin with the shown buffer you can set an offset for the tabline.
+vim.api.nvim_create_autocmd('BufWinEnter', {
+  pattern = '*',
+  callback = function()
+    if vim.bo.filetype == 'NvimTree' then
+      require'bufferline.api'.set_offset(31, 'FileTree')
+    end
+  end
+})
+
+vim.api.nvim_create_autocmd('BufWinLeave', {
+  pattern = '*',
+  callback = function()
+    if vim.fn.expand('<afile>'):match('NvimTree') then
+      require'bufferline.api'.set_offset(0)
+    end
+  end
+})
