ca40207b9bdfce8c5118f5950b2de3ee3378d0ae
Anton Vietrov <avietrov@users.noreply.github.com>
2022-11-19 18:47:52 +0100

    Fix telescope config.

--- nvim/lua/user/telescope.lua
+++ nvim/lua/user/telescope.lua
@@ -1,9 +1,11 @@
 local telescope = require "telescope"
 
 telescope.setup({
-  prompt_prefix = " ",
-  selection_caret = " ",
-  path_display = { "smart" },
+  defaults = {
+    prompt_prefix = " ",
+    selection_caret = " ",
+    path_display = { "smart" },
+  }
 })
 
 telescope.load_extension "projects"
