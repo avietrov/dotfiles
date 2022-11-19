d1c0d1bff2b660bb5f7701623de70359da98db58
Anton Vietrov <avietrov@users.noreply.github.com>
2022-11-19 19:53:02 +0100

    Setup telescop style.

--- nvim/lua/user/telescope.lua
+++ nvim/lua/user/telescope.lua
@@ -5,6 +5,13 @@ telescope.setup({
     prompt_prefix = " ",
     selection_caret = " ",
     path_display = { "smart" },
+  },
+
+  pickers = {
+    buffers = {
+      theme = 'dropdown',
+      previewer = false
+    }
   }
 })
 
@@ -12,3 +19,4 @@ telescope.load_extension "projects"
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<C-b>', builtin.buffers, {})
