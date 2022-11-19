17f2b5b784e48ceddc8daed843d98b338c95128b
Anton Vietrov <avietrov@users.noreply.github.com>
2022-11-19 11:37:42 +0100

    Use illuminate plugin.
    LunarVim/Neovim-from-scratch/pull/144

--- nvim/lua/user/lsp/handlers.lua
+++ nvim/lua/user/lsp/handlers.lua
@@ -18,19 +18,8 @@ local function lsp_keymaps(bufnr)
 end
 
 local function lsp_highlight_document(client)
-  -- Set autocommands conditional on server_capabilities
-  if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_exec(
-      [[
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]],
-      false
-    )
-  end
+  local illuminate = require('illuminate')
+  illuminate.on_attach(client)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
