e47dc076cb735aa09548c71f823a2d7d0efffc64
Anton Vietrov <avietrov@users.noreply.github.com>
2022-11-19 14:20:11 +0100

    Extend lua config.

--- nvim/lua/user/lsp/settings/sumneko_lua.lua
+++ nvim/lua/user/lsp/settings/sumneko_lua.lua
@@ -4,6 +4,15 @@ return {
       diagnostics = {
         globals = { "vim" },
       },
+      workspace = {
+        library = {
+          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+          [vim.fn.stdpath "config" .. "/lua"] = true,
+        },
+      },
+      telemetry = {
+        enable = false,
+      },
     },
   },
 }
