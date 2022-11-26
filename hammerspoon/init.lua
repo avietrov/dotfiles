691dc67bbc6d6c548637c4e4dc4004ea7ac17537
Anton Vietrov <avietrov@users.noreply.github.com>
2022-11-26 19:43:01 +0100

    Add basic hammerspoon.

---
+++ hammerspoon/init.lua
@@ -0,0 +1,54 @@
+local MEH = {"ctrl", "alt", "shift"}
+local HYPER = {"ctrl", "alt", "shift", "cmd"}
+
+hs.hotkey.bind(HYPER, "H", function ()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind(HYPER, "L", function ()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.w / 2 + max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind(HYPER, "K", function ()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind(HYPER, "J", function ()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.h / 2 + max.y
+  f.w = max.w
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
