19a0fd0bb4a214c00b156df443cf89c5ff3151dd
Anton Vietrov <avietrov@users.noreply.github.com>
2022-12-11 19:02:41 +0100

    Add vim keybinding to open live grep.

--- nvim/lua/user/telescope.lua
+++ nvim/lua/user/telescope.lua
@@ -19,4 +19,5 @@ telescope.load_extension "projects"
 
 local builtin = require "telescope.builtin"
 vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+vim.keymap.set("n", "<C-f>", builtin.live_grep, {})
 vim.keymap.set("n", "<C-b>", builtin.buffers, {})
