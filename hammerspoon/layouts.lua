90b889c48ea050389bf6134073ee0f8393886154
Anton Vietrov <avietrov@users.noreply.github.com>
2022-12-12 11:24:57 +0100

    Add work layout.

--- hammerspoon/layouts.lua
+++ hammerspoon/layouts.lua
@@ -13,3 +13,16 @@ hs.hotkey.bind(keys.HYPER, "1", function ()
   }
   hs.layout.apply(layout)
 end)
+
+
+
+hs.hotkey.bind(keys.HYPER, "2", function ()
+  local layout = {
+    {"Firefox",  nil, homeScreen, hs.layout.maximised, nil, nil},
+    {"iTerm2", nil, homeScreen, hs.layout.maximised, nil, nil},
+    {"Slack", nil, laptopScreen, hs.layout.maximised, nil, nil},
+    {"Spotify", nil, laptopScreen, hs.layout.maximised, nil, nil},
+    {"Things", nil, laptopScreen, hs.layout.maximised, nil, nil}
+  }
+  hs.layout.apply(layout)
+end)
