2a5a05b176d0a28dfe0629c0e632d6e1a57ec322
Anton Vietrov <avietrov@users.noreply.github.com>
2023-07-13 21:12:17 -0400

    Fix iTerm lunch.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,8 +2,6 @@ require('window')
 require('keybindings')
 require('layouts')
 
-hs.application.launchOrFocus("iTerm")
-
 hs.alert.show("Configuration Reloaded!")
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
