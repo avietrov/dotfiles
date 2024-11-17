52229aae152985bc11faf9b7b6ff27c8e3e112ae
Anton Vietrov <avietrov@users.noreply.github.com>
2024-11-16 22:47:53 -0500

    Remove hammerspoon.

--- hammerspoon/init.lua
+++
@@ -1,7 +0,0 @@
-require('window')
-require('keybindings')
-require('layouts')
-
-hs.alert.show("Configuration Reloaded!")
-hs.loadSpoon("ReloadConfiguration")
-spoon.ReloadConfiguration:start()
--- hammerspoon/keys.lua
+++
@@ -1,4 +0,0 @@
-local M = {}
-M.MEH = {"ctrl", "alt", "shift"}
-M.HYPER = {"ctrl", "alt", "shift", "cmd"}
-return M
