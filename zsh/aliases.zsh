b2cfa4f3842c00a2c2847480255036df624f644d
Anton Vietrov <avietrov@users.noreply.github.com>
2026-07-25 19:18:02 -0400

    zsh reconfigure

---
+++ zsh/aliases.zsh
@@ -0,0 +1,19 @@
+# Aliases
+alias reload='source $ZDOTDIR/.zshrc'
+alias rm='trash'
+alias vim='nvim'
+alias e='eza -l -h --no-permissions --no-user --icons --group-directories-first'
+
+
+# Git
+alias gap='git add -p'
+alias gb='git branch -v'
+alias gcm='git commit -m'
+alias gco='git checkout'
+alias gd='git diff'
+alias glg='git log --graph --decorate --oneline --abbrev-commit'
+alias gll='git log --oneline'
+alias gp='git push origin HEAD'
+alias gpp='git push -u origin HEAD'
+alias gs='git status -sb'
+
